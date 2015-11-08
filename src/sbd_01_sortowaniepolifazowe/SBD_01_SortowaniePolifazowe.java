3c296aaab760cf24be8c3d09933a3041f8966406
milleniumbug <wojas1222@gmail.com>
2015-11-08 17:34:38 +0100

    meh whatever

--- src/sbd_01_sortowaniepolifazowe/SBD_01_SortowaniePolifazowe.java
+++ src/sbd_01_sortowaniepolifazowe/SBD_01_SortowaniePolifazowe.java
@@ -1,5 +1,6 @@
 package sbd_01_sortowaniepolifazowe;
 
+import com.sun.istack.internal.logging.Logger;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,15 +8,18 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Consumer;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SBD_01_SortowaniePolifazowe {
 
     public static final int NULLS_ARE_BIGGER = -1;
     public static final int NULLS_ARE_SMALLER = 1;
+    private static final Logger logger = Logger.getLogger(SBD_01_SortowaniePolifazowe.class);
 
-    public static <T extends Comparable<T>> int compare(T left, T right, int null_importance) {
+    public static <T extends Comparable<T>> int compareNullable(T left, T right, int null_importance) {
         if (left == null && right == null) {
             return 0;
         }
@@ -35,32 +39,27 @@ public class SBD_01_SortowaniePolifazowe {
         }
     }
 
-    public static boolean dystrybuuj(List<Tape> tapes) throws IOException {
-        Iterator<Integer> dest_index_iter = Stream.iterate(2, x -> 2+(x-2+1)%2).iterator();
-        //Iterator<Integer> dest_index_iter = Fibonacci.fibonacciAlternatorStream(2, 3).iterator();
+    public static boolean dystrybuujNaturalne(List<Tape> tapes) throws IOException {
+        Iterator<Integer> dest_index_iter = Stream.iterate(2, x -> 2 + (x - 2 + 1) % 2).iterator();
         Integer current_destination = dest_index_iter.next();
         long liczba_serii_ogolem = 1;
 
         tapes.get(0).reset();
         tapes.get(1).reset();
-        tapes.get(0).retrieve();
-        tapes.get(1).retrieve();
         tapes.get(2).rewrite();
         tapes.get(3).rewrite();
 
         while (tapes.stream().limit(2).anyMatch(x -> x.top() != null)) {
-            System.out.println("(" + tapes.get(0).top() + " czy " + tapes.get(1).top() + ")");
 
-            int left_cmp = compare(tapes.get(0).top(), tapes.get(current_destination).top(), NULLS_ARE_SMALLER) < 0 ? 1 : 0;
-            int right_cmp = compare(tapes.get(1).top(), tapes.get(current_destination).top(), NULLS_ARE_SMALLER) < 0 ? 1 : 0;
-            int stacks_cmp = compare(tapes.get(0).top(), tapes.get(1).top(), NULLS_ARE_BIGGER) < 0 ? 1 : 0;
+            int left_cmp = compareNullable(tapes.get(0).top(), tapes.get(current_destination).top(), NULLS_ARE_SMALLER) < 0 ? 1 : 0;
+            int right_cmp = compareNullable(tapes.get(1).top(), tapes.get(current_destination).top(), NULLS_ARE_SMALLER) < 0 ? 1 : 0;
+            int stacks_cmp = compareNullable(tapes.get(0).top(), tapes.get(1).top(), NULLS_ARE_BIGGER) < 0 ? 1 : 0;
             final int to_adv = new int[]{1, 0, 0, 0, 1, 1, 1, 0}[(left_cmp << 2) | (right_cmp << 1) | (stacks_cmp << 0)];
-            String to_put = tapes.get(to_adv).top();
+            TapeSentinel<String> to_put = tapes.get(to_adv).top();
             tapes.get(to_adv).retrieve();
             assert to_put != null;
 
             if (left_cmp != 0 && right_cmp != 0) {
-                System.out.println("--- NOWA SERIA ---");
                 liczba_serii_ogolem++;
                 current_destination = dest_index_iter.next();
             }
@@ -69,24 +68,41 @@ public class SBD_01_SortowaniePolifazowe {
         return liczba_serii_ogolem == 1;
     }
 
-    public static void sortuj(Tape a1, Tape a2, Tape a3, Tape a4) throws IOException {
+    public static void sortujNaturalne(Tape a1, Tape a2, Tape a3, Tape a4) throws IOException {
         List<Tape> tapes = Arrays.asList(a1, a2, a3, a4);
-        int ilosc_przejsc = 1;
-        while (!dystrybuuj(tapes)) {
-            System.out.println("!!NOWE SORTOWANIE!!!");
-            Collections.rotate(tapes, 2);
-            ilosc_przejsc++;
+        List<Tape> tapes_in_original_order = new ArrayList<>(tapes);
+        try {
+            int ilosc_przejsc = 1;
+            while (!dystrybuujNaturalne(tapes)) {
+                for (Tape t : tapes) {
+                    t.close();
+                }
+                String komunikat = String.format("--- KOLEJNA FAZA ---\n"
+                        + "Liczba odczytów na każdą taśmę do tej pory: %s\n"
+                        + "Liczba zapisów na każdą taśmę do tej pory: %s\n",
+                        tapes_in_original_order.stream().map(x -> x.readCount()).collect(Collectors.toList()).toString(),
+                        tapes_in_original_order.stream().map(x -> x.writeCount()).collect(Collectors.toList()).toString());
+                logger.log(Level.INFO, komunikat);
+                Collections.rotate(tapes, 2);
+                ilosc_przejsc++;
+            }
+            logger.log(Level.INFO, String.format("Ilość przejść: %s\n", ilosc_przejsc));
+        } finally {
+            for (Tape t : tapes) {
+                t.close();
+            }
         }
-        System.out.printf("Ilość przejść: %s\n", ilosc_przejsc);
-        for(Tape t : tapes) t.close();
     }
 
-    public static void generateRandomFile(File f, long length) throws FileNotFoundException, IOException {
+    public static File generateRandomFile(File f, long length) throws FileNotFoundException, IOException {
         try (OutputStream os = new BufferedOutputStream(new FileOutputStream(f))) {
             Random rand = new Random();
-            String[] alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
-            rand.ints(0, alphabet.length)
-                    .mapToObj(x -> alphabet[x])
+            String lowalpha = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+            String highalpha = lowalpha.toUpperCase();
+            String digits = "0,1,2,3,4,5,6,7,8,9";
+            String[] all_letters = String.join(",", lowalpha, highalpha, digits).split(",");
+            rand.ints(0, all_letters.length)
+                    .mapToObj(x -> all_letters[x])
                     .limit(30 * length)
                     .forEachOrdered(x -> {
                         try {
@@ -96,75 +112,166 @@ public class SBD_01_SortowaniePolifazowe {
                         }
                     });
         }
+        return f;
     }
 
-    public static <T extends Comparable<T>> void dystrybuuj(Stream<T> input1, Stream<T> input2, Consumer<T> output1, Consumer<T> output2) {
-        ArrayList<Iterator<T>> input_iters = new ArrayList<>(2);
-        input_iters.add(input1.iterator());
-        input_iters.add(input2.iterator());
+    public static class ZczytywaczSerii {
 
-        ArrayList<Consumer<T>> output_iters = new ArrayList<>(2);
-        output_iters.add(output1);
-        output_iters.add(output2);
+        Tape tasma;
+        boolean koniec_serii = false;
+        TapeSentinel<String> poprzedni;
+        IOException exception;
 
-        ArrayList<T> input_items = new ArrayList<>(2);
-        input_items.add(input_iters.get(0).hasNext() ? input_iters.get(0).next() : null);
-        input_items.add(input_iters.get(1).hasNext() ? input_iters.get(1).next() : null);
+        public ZczytywaczSerii(Tape tasma) {
+            this.tasma = tasma;
+        }
 
-        ArrayList<T> output_items = new ArrayList<>(2);
-        output_items.add(null);
-        output_items.add(null);
+        IOException ioException() {
+            return exception;
+        }
 
-        Iterator<Integer> dest_index_iter = Fibonacci.fibonacciAlternatorStream(0, 1).iterator();
-        Integer current_destination = dest_index_iter.next();
-        while (input_items.stream().anyMatch(x -> x != null)) {
-            //System.out.println("(" + input_items.get(0) + " czy " + input_items.get(1) + ")");
-
-            int left_cmp = compare(input_items.get(0), output_items.get(current_destination), NULLS_ARE_SMALLER);
-            int right_cmp = compare(input_items.get(1), output_items.get(current_destination), NULLS_ARE_SMALLER);
-            int stacks_cmp = compare(input_items.get(0), input_items.get(1), NULLS_ARE_BIGGER);
-            int to_adv = new int[]{1, 0, 0, 0, 1, 1, 1, 0}[(left_cmp < 0 ? 1 : 0) * 4 + (right_cmp < 0 ? 1 : 0) * 2 + (stacks_cmp < 0 ? 1 : 0) * 1];
-            T to_put = input_items.get(to_adv);
-            input_items.set(to_adv, input_iters.get(to_adv).hasNext() ? input_iters.get(to_adv).next() : null);
-            assert to_put != null;
-
-            if (left_cmp < 0 && right_cmp < 0) {
-                //System.out.println("--- NOWA SERIA ---");
-                current_destination = dest_index_iter.next();
+        TapeSentinel<String> next() {
+            try {
+                if (koniec_serii) {
+                    return null;
+                }
+                poprzedni = tasma.top();
+                tasma.retrieve();
+                if (compareNullable(poprzedni, tasma.top(), NULLS_ARE_SMALLER) > 0) {
+                    koniec_serii = true;
+                    return new TapeSentinel<>(1);
+                }
+                return tasma.top();
+            } catch (IOException ex) {
+                this.exception = ex;
             }
-            output_iters.get(current_destination).accept(to_put);
-            output_items.set(current_destination, to_put);
+            return null;
+        }
+
+        boolean hasNext() {
+            return !koniec_serii;
+        }
+
+        TapeSentinel<String> current() {
+            return koniec_serii ? null : tasma.top();
         }
     }
 
+    public static void scalSerie(List<Tape> input, Tape out) throws IOException {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("there must be at least one input tape");
+        }
+        List<ZczytywaczSerii> in = input.stream()
+                .map(ZczytywaczSerii::new)
+                .collect(Collectors.toList());
+        while (in.stream().anyMatch(ZczytywaczSerii::hasNext)) {
+            ZczytywaczSerii which = in.stream()
+                    .reduce((x, y) -> compareNullable(x.current(), y.current(), NULLS_ARE_BIGGER) < 0 ? x : y)
+                    .get();
+            out.receive(which.current());
+            which.next();
+        }
+    }
+
+    public static void poczatkowaDystrybucja(Tape in, Tape out1, Tape out2) throws IOException {
+        in.reset();
+        out1.rewrite();
+        out2.rewrite();
+        final Iterator<Tape> dest_it = Fibonacci.fibonacciAlternatorStream(out1, out2).iterator();
+        Tape dest = dest_it.next();
+        long before, after;
+        while (in.top() != null) {
+            do {
+                before = dest.runCount();
+                scalSerie(Arrays.asList(in), dest);
+                after = dest.runCount();
+            } while (before == after);
+            dest = dest_it.next();
+        }
+        // uzupełnij dummiesami
+        do {
+            dest.addDummyRuns(1);
+        } while (dest == dest_it.next());
+    }
+
+    public static Tape kolejneDystrybucje(Tape in1, Tape in2, Tape out) throws IOException {
+        in1.reset();
+        in2.reset();
+        List<Tape> tapes = Arrays.asList(in1, in2, out);
+        final int destination_index = 2;
+        while (true) {
+            System.out.println(tapes.get(0).runCount());
+            System.out.println(tapes.get(1).runCount());
+            System.out.println(tapes.get(2).runCount());
+            System.out.println();
+            tapes.get(destination_index).rewrite();
+            List<Tape> inputs = tapes.subList(0, destination_index);
+            do {
+                scalSerie(inputs, tapes.get(destination_index));
+            } while (inputs.stream().allMatch(x -> x.top() != null));
+            if (inputs.stream().allMatch(x -> x.top() == null)) {
+                return tapes.get(destination_index);
+            }
+            int finished_index = tapes.indexOf(inputs.stream().filter(x -> x.top() == null).findFirst().get());
+            Collections.swap(tapes, finished_index, destination_index);
+            tapes.get(finished_index).reset();
+        }
+    }
+
+    public static File createFileFromKeyboardInput(File f) throws IOException {
+        System.out.println("Please input lines in sequence. EOF or empty line terminates input");
+        Scanner s = new Scanner(System.in);
+        Tape t = new Tape(f, 8192);
+        try {
+            t.rewrite();
+            while (s.hasNextLine()) {
+                final String line = s.nextLine();
+                if (line.equals("")) {
+                    break;
+                }
+                if (line.length() <= 30) {
+                    t.receive(new TapeSentinel<>(line));
+                } else {
+                    System.err.println("!!! Invalid input length: please input less than 30 chars - this line will be ignored");
+                }
+            }
+        } finally {
+            t.close();
+        }
+        return f;
+    }
+
     public static void main(String[] args) throws Exception {
-        //dystrybuuj(Stream.of(1, 9, 3), Stream.of(4, 5, 6, 7, 2), (x) -> { System.out.println(x); }, (x) -> { System.out.println("          "+x); });
-        int bufor = 4096;
-        generateRandomFile(new File("abcdef.whatever"), 5000);
-        Tape a = new Tape(new File("aaa.whatever"), bufor);
-        File f = new File("bbb.whatever");
-        f.createNewFile();
-        sortuj(a, new Tape(f, bufor), new Tape(new File("ccc.whatever"), bufor), new Tape(new File("ddd.whatever"), bufor));
-        /*System.out.println("aaaaaa");
-         dystrybuuj(new Random().ints(-100, 100).map(x -> x*10+0).limit(75).boxed(),
-         IntStream.empty().boxed(),
-         (x) -> { System.out.println(x); },
-         (x) -> { System.out.println("          "+x); });
-         assert compare(null, null, NULLS_ARE_BIGGER) == 0;
-         assert compare(null, "aaaa", NULLS_ARE_BIGGER) > 0;
-         assert compare(null, "aaaa", NULLS_ARE_SMALLER) < 0;
-         assert compare("aaaa", null, NULLS_ARE_BIGGER) < 0;
-         assert compare("aaaa", null, NULLS_ARE_SMALLER) > 0;
-         assert compare("aaaa", "bbbb", NULLS_ARE_BIGGER) == "aaaa".compareTo("bbbb");
-         CountedInputStream cis = new CountedInputStream(new FileInputStream(new File("asdf")));
-         InputStream is = new BufferedInputStream(cis);
-         byte[] buffer = new byte[30];
-         for(int i = 0; i < 50; ++i)
-         {
-         int read = is.read(buffer, 0, buffer.length);
-         assert read == buffer.length;
-         System.out.println(Arrays.toString(buffer));
-         }
-         System.out.println(cis.readCount());*/
+        List<String> arguments = Arrays.asList(args);
+        File input_file, output_file;
+        Iterator<String> argsiter = arguments.iterator();
+        switch (argsiter.next()) {
+            case "K":
+                input_file = createFileFromKeyboardInput(new File("key.whatever"));
+                output_file = new File(argsiter.next());
+                break;
+            case "F":
+                input_file = new File(argsiter.next());
+                output_file = new File(argsiter.next());
+                break;
+            case "R":
+                input_file = generateRandomFile(new File("random.whatever"), Long.valueOf(argsiter.next()));
+                output_file = new File(argsiter.next());
+                break;
+            default:
+                return;
+        }
+        int bufor = argsiter.hasNext() ? Integer.valueOf(argsiter.next()) : 4096;
+
+        final Tape a = new Tape(input_file, bufor);
+        final Tape b;
+        {
+            File f = new File("bbb.whatever");
+            f.createNewFile();
+            b = new Tape(f, bufor);
+        }
+        final Tape c = new Tape(output_file, bufor);
+        final Tape d = new Tape(new File("ddd.whatever"), bufor);
+        sortujNaturalne(a, b, c, d);
     }
 }
